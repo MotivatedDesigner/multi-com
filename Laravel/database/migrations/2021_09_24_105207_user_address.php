54f7e2a236385956aadb819eeefb59ee3882146e
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-09-24 11:55:13 +0100

    moving type of address to the pivot table

--- Laravel/database/migrations/2021_09_24_105207_user_address.php
+++ Laravel/database/migrations/2021_09_24_105207_user_address.php
@@ -17,6 +17,7 @@ class UserAddress extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('address_id')->unsigned();
+            $table->enum('type',['generated','shipping','billing'])->default('generated');
             $table->timestamps();
         });
     }
