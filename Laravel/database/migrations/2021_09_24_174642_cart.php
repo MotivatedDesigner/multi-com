e6bd3aa30e2b5e95c7d13b0210ce617cb3b42e8f
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-09-24 18:50:39 +0100

    db orders & cart

---
+++ Laravel/database/migrations/2021_09_24_174642_cart.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Cart extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->bigInteger('cart_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->bigInteger('order_id')->unsigned();
+            $table->integer('quantity',5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
