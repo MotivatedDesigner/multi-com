c2bf86d85ac5b138bd0af54e0bbb74cff32f9134
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-09-24 11:07:29 +0100

    adding shop migration & model

---
+++ Laravel/database/migrations/2021_09_24_100356_create_shops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50)->unique();
+            $table->string('photo',50);
+            $table->string('summary');
+            $table->text('description');
+            $table->enum('status',['active','inactive'])->default('active');
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
