013314fd5735203082f649781d22b43d7357551e
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-09-24 19:15:55 +0100

    Seeding admin,vendor & customer users

--- Laravel/database/seeders/UserSeeder.php
+++ Laravel/database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,28 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::insert(
+            //admin
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin',
+                'password' => Hash::make('admin'),
+                'roll' => 'admin',
+            ],
+            //vendor
+            [
+                'name' => 'vendor',
+                'email' => 'vendor@vendor',
+                'password' => Hash::make('vendor'),
+                'roll' => 'vendor',
+            ],
+            //customer
+            [
+                'name' => 'customer',
+                'email' => 'customer@customer',
+                'password' => Hash::make('customer'),
+                'roll' => 'customer',
+            ],
+        );
     }
 }
